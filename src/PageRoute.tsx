97ace497fcc34afe23a0af48cc3ab4ac1658917f
Arpita Muchewad <arpita.muchewad@iauro.com>
2023-10-12 14:24:35 +0530

    Arpita deployment

--- src/PageRoute.tsx
+++ src/PageRoute.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductList from './Components/ProductList';
-// import { Dashboard } from '@mui/icons-material';
-import ProductDetails from './Components/ProductDetails';
+
+
 import Dashboard from './Components/Dashboard';
 const PageRoute = () => {
   return (
@@ -10,7 +10,6 @@ const PageRoute = () => {
         <Routes>
             <Route element={<Dashboard/>} path=''/>
             <Route element={<ProductList/>} path='/ProductsList'/>
-            {/* <Route element={<ProductDetails imageUrl={''} name={''} originalPrice={''} discountedPrice={''} ratings={''}/>} path='/ProductsDetails'/> */}
         </Routes>
     </Router>
   )
